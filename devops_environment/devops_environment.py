7bf4b0f511bfb7629abe2f455e3ab207bd01aa9d
Steven Miller <sjmiller609@gmail.com>
2019-02-26 02:37:21 +0000

    wip

--- devops_environment/devops_environment.py
+++ devops_environment/devops_environment.py
@@ -3,6 +3,7 @@
 """Main module."""
 
 import os
+import sys
 import docker
 
 home = os.environ.get("HOME")
@@ -24,15 +25,36 @@ def pull_image(image='sjmiller609/stelligent_development', version="latest"):
     docker_client.images.pull(image, tag=version)
     print("Done pulling image.")
 
+def _get_mount_commands_for_directories(directories):
+
 def print_run_command():
+    directories = ["{}/.gitconfig".format(home),
+                   "{}/.ssh".format(home),
+                   "/var/run/docker.socket"]
+    mount_commands = _get_mount_commands_for_directories(directories)
+
+    if not os.path.isfile(mount_git_config):
+        mount_git_config = ""
+    else:
+        mount_git_config = "-v " + mount_git_config
+
+    mount_ssh_config = "{}/.ssh".format(home)
+    if not os.path.isfile(mount_ssh_config):
+        mount_ssh_config = ""
+    else:
+        mount_ssh_config = "-v " + mount_ssh_config
+
+    mount_docker_socket = "/var/run/docker.sock"
+    if not os.path.isfile(mount_docker_socket):
+        mount_docker_socket = ""
+    else:
+        mount_docker_socket = "-v " + mount_docker_socket
+
     print("""
-    docker run -it --rm \
-      -v {home}/.gitconfig:/root/.gitconfig \
-      -v /var/run/docker.sock:/var/run/docker.sock \
-      -v {home}/.ssh/:/root/.ssh/ \
+    docker run -it --rm {mount_commands}
       -v {home}/development_backup:/root/shared \
       sjmiller609/stelligent_development /bin/bash
-    """.format(home=home))
+    """.format(mount_commands=mount_commands, home=home))
 
 def setup():
     pull_image()
